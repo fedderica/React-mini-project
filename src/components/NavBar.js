3909eb8ba825a1489022a408fbdd20b2529611ab
fedderica <fedderica.bartoli@gmail.com>
2023-03-06 09:57:08 -0300

    Primeras estructuras

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+const NavBar = () => {
+    return( 
+<nav className="navbar bg-dark text-light nb-5">
+    <div className="container-fluid">
+        <h3 className="mx-auto"> Prediccion Meteorologica </h3>
+    </div>
+</nav>
+    );
+};
+export default NavBar;
